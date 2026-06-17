1839d6d48f87ec0eb6d66f52d780899e2b099440
thatcrispybacon <280373692+thatcrispybacon@users.noreply.github.com>
2026-06-17 12:59:55 +0100

    Update and rename enfoce.js to enforce.js

---
+++ unpacked/enforce.js
@@ -0,0 +1,7 @@
+let currentUrl = window.location.href()
+if currentUrl == "https://www.duckduck.go" {
+  // Parse a full URL string
+  const url = new URL("duckduckgo.com");
+  url.protocol = "https://"
+  url.hostname = "safe.duckduckgo.com"; // Changes DNS to safe search
+}
