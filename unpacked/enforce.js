fd0b362e231e60dd139c339b5ec44bd9637bba3b
thatcrispybacon <280373692+thatcrispybacon@users.noreply.github.com>
2026-06-26 08:53:06 +0100

    Update enforce.js

--- unpacked/enforce.js
+++ unpacked/enforce.js
@@ -4,6 +4,9 @@ let currentUrl = window.location.href()
 if currentUrl == "https://www.duckduck.go" {
   // Parse a full URL string
   const url = new URL("duckduckgo.com");
-  url.protocol = "https://"
-  url.hostname = "safe.duckduckgo.com"; // Changes DNS to safe search
+  url.protocol = "https://";
+  url.hostname = `safe.${url.hostname}`; // Changes DNS to safe search
+  url.searchParams.set("pk", 1);
+  const safeURL  = url.toString();
+  window.location.assign(safeURL)
 }
